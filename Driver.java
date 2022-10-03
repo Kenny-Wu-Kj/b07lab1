af07cef686727399ef562a044e2c68182c60cb8e
Kenny Wu <kennywu@KennydeMacBook-Pro.local>
2022-10-02 21:56:58 -0400

    modified Polynomial.java and Driver.java”

--- Driver.java
+++ Driver.java
@@ -1,17 +1,68 @@
+import java.util.concurrent.CountDownLatch;
+import java.io.*;
+
 public class Driver { 
-    public static void main(String [] args) { 
-     Polynomial p = new Polynomial(); 
-     System.out.println(p.evaluate(3)); 
-     double [] c1 = {6,0,0,5}; 
-     Polynomial p1 = new Polynomial(c1); 
-     double [] c2 = {0,-2,0,0,-9}; 
-     Polynomial p2 = new Polynomial(c2); 
-     Polynomial s = p1.add(p2); 
-     System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-     if(s.hasRoot(1)) 
-      System.out.println("1 is a root of s"); 
-     else 
-      System.out.println("1 is not a root of s"); 
+    public static void main(String [] args) throws IOException{ 
+    File f = new File("/Users/kennywu/b07lab1/file.txt"); //Change to your dir
+    
+    System.out.println("-----------------------------------------" + 
+                      "\n" + "Test for Polynomial Class: " + "\n");
+
+    //Test constructor Polynomial(File f)
+    Polynomial p = new Polynomial(f);
+    System.out.println("P1: " + p);
+    System.out.println();
+    
+    //Test constructor Polynomial(double[] c, int[] e) 
+    //    - Precon: each exponent number has to be an unique integer
+    double [] c1 = {6,5}; 
+    int [] e1 = {3,1};
+    Polynomial p1 = new Polynomial(c1, e1);
+    System.out.println("P2: " + p1);
+    System.out.println();
+
+    //Test constructor Polynomial()
+    Polynomial p2 = new Polynomial();
+    System.out.println("P3: " + p2);
+    System.out.println();
+
+    //Test method double evaluate(double x)
+    System.out.println("Result of P1(x=0.5): " + "\n" + p.evaluate(0.5));
+    System.out.println("Result of P1(x=1): " + "\n" + p.evaluate(1.0));
+    System.out.println("Result of P1(x=2): " + "\n" + p.evaluate(2.0));
+    System.out.println();
+
+    //Test method boolean hasRoot(double r)
+    System.out.println("Whether x=0 is a root of P1: " + p.hasRoot(0));
+    System.out.println("Whether x=1.5 is a root of P1: " + p.hasRoot(1.5));
+    System.out.println();
+
+    //Test method Polynomial add(Polynomial p)
+    System.out.println("Result of Addition (P1 + P2): " + "\n" + p.add(p1));
+    System.out.println();
+    
+    //Test method Polynomial multiply(Polynomial p)
+    System.out.println("Result of Multiplication (P1 * P2): " + "\n" + p.multiply(p1));
+    System.out.println();
+
+    //Test method void saveToFile(String filename)
+    p1.saveToFile("/Users/kennywu/b07lab1/file_output.txt"); //Change to your dir
+    System.out.println();
+    
+    //These are some public supplementary methods Polynomial class
+    //Test void bubbleSort();
+    p1.bubbleSort();
+    System.out.println("Result of bubbleSorting P2: " + "\n" + p1);
+    System.out.println();
+
+    //Test void cleaner() - Pre: Calling object can't be empty
+    double [] c2 = {-6,0,2}; 
+    int [] e2 = {3,1,4};
+    Polynomial p3 = new Polynomial(c2, e2);
+    System.out.println("New Polynomial: " + p3);
+    p3.cleaner();
+    System.out.println("After calling cleaner(): " + "\n" + p3);
+    System.out.println();
     } 
    } 
     
